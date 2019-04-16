a35e5b9ad6a92abd7df83b26f2acc6b8e9fd8c60
Vikram Shenoy <vikramshenoy97@gmail.com>
2019-04-16 21:15:44 +0530

    Improved

--- test.py
+++ test.py
@@ -10,8 +10,21 @@ wgan = WassersteinGAN(epochs=6000, verbose=True)
 wgan.fit(X, load_models=False)
 wgan.train(sample_interval=50)
 wgan.sample_images(grid=[5,5])
-"""
+
 # Use Pre-Trained Models.
 wgan = WassersteinGAN(verbose=True)
+wgan.fit(X, load_models=True, generator_file="Trained_Models/Generator.h5", critic_file="Trained_Models/Critic.h5")
+wgan.sample_images(grid=[9,9], name="Final", random=False)
+"""
+
+wgan = WassersteinGAN(verbose=True)
+for i in range(0, 93000, 3000):
+    if(i == 0):
+        wgan.fit(X, load_models=False)
+        wgan.sample_images(grid=[9,9], name=i, random=False)
+    else:
+        wgan.fit(X, load_models=True, generator_file="Trained_Models/Backup/Generator_"+str(i)+".h5", critic_file="Trained_Models/Backup/Critic_"+str(i)+".h5")
+        wgan.sample_images(grid=[9,9], name=i, random=False)
+        
 wgan.fit(X, load_models=True, generator_file="Trained_Models/Generator.h5", critic_file="Trained_Models/Critic.h5")
 wgan.sample_images(grid=[9,9], name="Final", random=False)
